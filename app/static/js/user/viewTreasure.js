fc82a7654cbb9ecf11b13268bcb741da98f0bc55
Mikey Badr <mfbadr@gmail.com>
2014-08-21 12:06:05 -0500

    added pin to treasure view

--- app/static/js/user/viewTreasure.js
+++ app/static/js/user/viewTreasure.js
@@ -8,13 +8,21 @@
 
   $(document).ready(function(){
     directionsDisplay = new google.maps.DirectionsRenderer();
-    var lat = parseFloat($('#data').attr('data-lat')),
-        lng = parseFloat($('#data').attr('data-lng'));
+    var lat  = parseFloat($('#data').attr('data-lat')),
+        name = $('#data').attr('data-name'),
+        lng  = parseFloat($('#data').attr('data-lng'));
     initMap(lat,lng,8);
+    addMarker(lat, lng, name);
+
     //directionsDisplay.setMap(map);
     //var locations = getLocations();
     //calcRoute(locations);
   });
+
+  function addMarker(lat, lng, name){
+    var latLng = new google.maps.LatLng(lat, lng);
+    new google.maps.Marker({map: map, position: latLng, title: name, animation:google.maps.Animation.DROP});
+  }
 
   function initMap(lat, lng, zoom){
     var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
