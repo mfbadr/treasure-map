c4025eaccc3a4a898d2e9b0fa0174de5972a4e59
Mikey Badr <mfbadr@gmail.com>
2014-08-21 13:09:05 -0500

    added tag filtering

--- app/helpers/link-builder.js
+++ app/helpers/link-builder.js
@@ -1,5 +1,32 @@
 'use strict';
 
+var _ = require('lodash');
+
+exports.url = function(query, key, value, text){
+  var q = _.cloneDeep(query),
+      link = '<a href="/treasures?';
+
+  q[key] = value;
+
+  var properties = Object.keys(q).map(function(prop){
+    return prop + '=' + q[prop];
+  });
+
+  link += properties.join('&');
+
+  link += '">' + text + '</a>';
+  return link;
+};
+
+exports.tags = function(query, tags){
+  var links = tags.map(function(tag){
+    return exports.url({}, 'tag', tag, tag)
+  });
+
+  return links.join(',');
+};
+
+
 exports.difficulty = function(dif){
   switch(dif){
     case 1:
@@ -17,3 +44,5 @@ exports.sort = function(query, name, display){
        link = '<a href="/treasures?sort=' + name + '&order=' + order + tag +'">'+display+'</a>';
   return link;
 };
+
+
